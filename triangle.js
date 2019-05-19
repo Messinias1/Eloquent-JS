09dab94c917eedb1be4b862e30166513fa001c22
Carl Kakisis <Messinias1@gmail.com>
2019-05-18 20:05:21 -0400

    first commit

---
+++ triangle.js
@@ -0,0 +1,8 @@
+// let triangle = ["#", "##", "###", "####", "#####", "######", "#######"]
+
+// for (let i = 0; i <= triangle.length; i++) {
+// 	console.log(triangle[i])
+// }
+
+for (let triangle = "#"; triangle.length < 8; triangle += "#")
+  console.log(triangle);
